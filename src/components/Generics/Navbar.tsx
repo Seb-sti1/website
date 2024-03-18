c53977c078e95d0c2b7ef3909c9fd15be4899395
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2024-03-18 21:42:42 +0100

    Simplify Navbar

--- src/components/Generics/Navbar.tsx
+++ src/components/Generics/Navbar.tsx
@@ -1,52 +1,24 @@
 import { FC } from "react";
-import styled from "styled-components";
-import { NavLink as Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-
-const Nav = styled.nav`
-  height: 85px;
-  display: flex;
-  justify-content: center;
-  padding: 0.2rem calc((100vw - 1200px) / 2);
-  z-index: 12;
-`;
-
-const NavLink = styled(Link)`
-  color: var(--text-color);
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  padding: 0 1rem;
-  height: 100%;
-  cursor: pointer;
-  font-family: "Julius Sans One", sans-serif;
-
-  &.active {
-    text-decoration: underline solid var(--text-color) 2px;
-  }
-`;
-
-const NavMenu = styled.div`
-  display: flex;
-  align-items: center;
-  margin-right: -24px;
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
-`;
+import "./styles/nabar.scss";
 
 const Navbar: FC = () => {
   const { t } = useTranslation();
 
   return (
     <>
-      <Nav>
-        <NavMenu>
-          <NavLink to="/">{t("nav.home")}</NavLink>
-          <NavLink to="/education">{t("nav.education")}</NavLink>
-          <NavLink to="/projects">{t("nav.projects")}</NavLink>
-        </NavMenu>
-      </Nav>
+      <nav className="navbar">
+        <NavLink className="link" to="/">
+          {t("nav.home")}
+        </NavLink>
+        <NavLink className="link" to="/education">
+          {t("nav.education")}
+        </NavLink>
+        <NavLink className="link" to="/projects">
+          {t("nav.projects")}
+        </NavLink>
+      </nav>
     </>
   );
 };
